652b58b57151010513a834b82aa11c24bdf2ff18
harshdongre2810 <dongreharsh53@gmail.com>
2026-02-06 22:10:54 +0530

    add script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(() => {
+      document.getElementById("loginBox").style.display = "none";
+      document.getElementById("chatBox").style.display = "block";
+    })
+    .catch(err => alert(err.message));
+}
+
+function send() {
+  const input = document.getElementById("userInput").value;
+  document.getElementById("chat").innerHTML += "<p><b>You:</b> " + input + "</p>";
+  document.getElementById("chat").innerHTML += "<p><b>Shejal:</b> 😊 मैं तुम्हारी बात समझ रही हूँ</p>";
+}
